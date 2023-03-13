ae085a6dcbef77434c11e246eb75e2300dc435bb
Aleksander Cvetkov <aleksandar.k.tsvetkov@gmail.com>
2023-03-13 20:04:15 +0200

    Adding original data (to the database), config file, uuid library, favicon.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+// import SendOriginalDataClubsComponent from "./assets/originalData/ordiginalData";
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      {/* <SendOriginalDataClubsComponent /> */}
     </div>
   );
 }
---
+++ src/util/config.js
@@ -0,0 +1,12 @@
+export const clubNames = [
+  "Sofia Center",
+  "Sofia Lyulin",
+  "Sofia Mladost",
+  "Sofia Pavlovo",
+  "Plovdiv Trakia",
+  "Plovdiv Center",
+  "Burgas Central Park",
+  "Varna Center",
+  "Varna Planet Mall",
+  "Varna Levski",
+];
