3bd796c8095490bbf3f4f90a108525c2f91bbeb1
Aleksander Cvetkov <aleksandar.k.tsvetkov@gmail.com>
2023-04-01 17:52:23 +0300

    Adding more original data to the database.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import ClubDetailsPage from "./pages/ClubDetailsPage/ClubDetailsPage";
 import ClubTimetablePage from "./pages/ClubTimetablePage/ClubTimetablePage";
 import { useSelector } from "react-redux";
+// import SendOriginalDataClubsComponent from "./util/ordiginalData";
 function App() {
   const user = useSelector((state) => state.activeUser);
   const router = createBrowserRouter([
@@ -39,7 +40,7 @@ function App() {
   ]);
   return (
     <RouterProvider router={router}>
-      <div className="App">{/* <SendOriginalDataClubsComponent /> */}</div>
+      <div className="App">{/* {<SendOriginalDataClubsComponent />} */}</div>
     </RouterProvider>
   );
 }
