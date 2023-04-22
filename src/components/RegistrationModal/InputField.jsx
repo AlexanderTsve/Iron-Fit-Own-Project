30d6e2be5cfc98368a6bdaf46a6e83304e1fd372
Aleksander Cvetkov <aleksandar.k.tsvetkov@gmail.com>
2023-04-22 22:32:56 +0300

    Registration modal logic and design improved.

--- src/components/RegistrationModal/InputField.jsx
+++ src/components/RegistrationModal/InputField.jsx
@@ -2,7 +2,23 @@ import Form from "react-bootstrap/Form";
 import InputIcon from "./InputIcon";
 import InputInstruction from "./InputInstruction";
 import { Fragment } from "react";
+import { InputGroup } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEyeSlash, faEye } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import styles from "./InputField.module.scss";
 const InputField = (props) => {
+  const [toggleHover, setToggleHover] = useState(false);
+  const [visiblePsw, setVisiblePsw] = useState(false);
+  const hoverHandler = () => {
+    setToggleHover(true);
+  };
+  const unhoverHandler = () => {
+    setToggleHover(false);
+  };
+  const toggleVisiblePswHandler = () => {
+    setVisiblePsw(!visiblePsw);
+  };
   return (
     <Fragment>
       <Form.Group className="mb-3" controlId={props.controlId}>
@@ -13,16 +29,49 @@ const InputField = (props) => {
             isValidInput={props.isValidInput}
           />
         </Form.Label>
-        <Form.Control
-          required={props.required}
-          type={props.type}
-          placeholder={props.placeholder}
-          ref={props.reference}
-          autoComplete={props.autoComplete}
-          onChange={props.onChange}
-          onFocus={props.onFocus}
-          onBlur={props.onBlur}
-        />
+        <InputGroup>
+          <Form.Control
+            required={props.required}
+            type={
+              props.placeholder === "Password..." && visiblePsw
+                ? "text"
+                : props.type
+            }
+            placeholder={props.placeholder}
+            ref={props.reference}
+            autoComplete={props.autoComplete}
+            onChange={props.onChange}
+            onFocus={props.onFocus}
+            onBlur={props.onBlur}
+            className={
+              props.placeholder === "Password..."
+                ? styles["password-input"]
+                : styles["reg-input"]
+            }
+          />
+          {props.placeholder === "Password..." ? (
+            <InputGroup.Text>
+              <span
+                className={styles.icon}
+                onMouseEnter={hoverHandler}
+                onMouseLeave={unhoverHandler}
+                onClick={toggleVisiblePswHandler}
+              >
+                <FontAwesomeIcon
+                  className="eye-icon"
+                  icon={visiblePsw ? faEye : faEyeSlash}
+                  style={
+                    toggleHover
+                      ? { color: "green", cursor: "pointer" }
+                      : { color: "white", cursor: "pointer" }
+                  }
+                />
+              </span>
+            </InputGroup.Text>
+          ) : (
+            ""
+          )}
+        </InputGroup>
       </Form.Group>
       <InputInstruction
         showInstruction={props.showInstruction}
